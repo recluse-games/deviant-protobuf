6d3717a7300fa2dbdcc08e805f6e217b8ae9f0fe
cmdallas <self@chrisdallas.tech>
2020-05-02 19:10:56 -0700

    ci(genproto_ci): install protoc-gen-go

--- genproto_ci.sh
+++ genproto_ci.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 # This should only be run by a robot
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOROOT:$GOPATH:$GOBIN
+
 PROTOC_ZIP=protoc-3.7.1-linux-x86_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
 rm -f $PROTOC_ZIP
+go install google.golang.org/protobuf/cmd/protoc-gen-go
 
 protoc --proto_path='protobuf/' --go_out=plugins=grpc:genproto/go/ protobuf/*
 protoc --proto_path='protobuf/' --csharp_out=genproto/cs/ protobuf/*
