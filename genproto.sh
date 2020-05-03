0b7b1a6f75415acddfbbdddeeedba62d2490b54e
cmdallas <self@chrisdallas.tech>
2020-05-02 18:45:16 -0700

    build(genproto): add csharp directive and make executable

--- genproto.sh
+++ genproto.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-protoc --proto_path='protobuf/' --go_out=plugins=grpc:genproto/ protobuf/*
+protoc --proto_path='protobuf/' --go_out=plugins=grpc:genproto/go/ protobuf/*
+protoc --proto_path='protobuf/' --csharp_out=genproto/cs/ protobuf/*
