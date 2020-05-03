469618473190dd45ab221b45a8127b56a52515ee
cmdallas <self@chrisdallas.tech>
2020-05-02 19:20:23 -0700

    gh actions testing

--- genproto_ci.sh
+++ genproto_ci.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 # This should only be run by a robot
-PROTOC_ZIP=protoc-3.7.1-linux-x86_64.zip
-curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
-sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
-rm -f $PROTOC_ZIP
-
-echo $GOROOT
-go install google.golang.org/protobuf/cmd/protoc-gen-go
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+sudo curl -LO $PB_REL/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
+sudo unzip protoc-3.11.4-linux-x86_64.zip -d $HOME/.local
+export PATH="$PATH:$HOME/.local/bin"
+go get github.com/golang/protobuf/protoc-gen-go
+export PATH="$PATH:$(go env GOPATH)/bin"
 
 protoc --proto_path='protobuf/' --go_out=plugins=grpc:genproto/go/ protobuf/*
 protoc --proto_path='protobuf/' --csharp_out=genproto/cs/ protobuf/*
