0ad5651af51aa88a75d6bf8160ebfda1a1b629e1
cmdallas <self@chrisdallas.tech>
2020-05-02 19:05:55 -0700

    ci(genproto_ci): use sudo

--- genproto_ci.sh
+++ genproto_ci.sh
@@ -3,8 +3,8 @@
 # This should only be run by a robot
 PROTOC_ZIP=protoc-3.7.1-linux-x86_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
-unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
+sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
 rm -f $PROTOC_ZIP
 
 protoc --proto_path='protobuf/' --go_out=plugins=grpc:genproto/go/ protobuf/*
