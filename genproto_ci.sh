173112a1d8bb862a4aca5bcc12334df0ca4aceea
cmdallas <self@chrisdallas.tech>
2020-05-02 19:36:28 -0700

    ci(genproto_ci): actually delete protoc zip

--- genproto_ci.sh
+++ genproto_ci.sh
@@ -2,9 +2,10 @@
 
 # This should only be run by a robot
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
-sudo curl -LO $PB_REL/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
+PROTOC_ZIP="protoc-3.11.4-linux-x86_64.zip"
+sudo curl -LO $PB_REL/download/v3.11.4/$PROTOC_ZIP
 sudo unzip protoc-3.11.4-linux-x86_64.zip -d $HOME/.local
-rm -f ${PB_REL}
+rm -f $PROTOC_ZIP
 export PATH="$PATH:$HOME/.local/bin"
 go get github.com/golang/protobuf/protoc-gen-go
 export PATH="$PATH:$(go env GOPATH)/bin"∂∂∂
