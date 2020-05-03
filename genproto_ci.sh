e4ccbdbefb85adac3a96dce3e0f3afe529b4bb6b
cmdallas <self@chrisdallas.tech>
2020-05-02 19:39:23 -0700

    ci(genproto_ci): ∂∂∂

--- genproto_ci.sh
+++ genproto_ci.sh
@@ -8,7 +8,7 @@ sudo unzip protoc-3.11.4-linux-x86_64.zip -d $HOME/.local
 rm -f $PROTOC_ZIP
 export PATH="$PATH:$HOME/.local/bin"
 go get github.com/golang/protobuf/protoc-gen-go
-export PATH="$PATH:$(go env GOPATH)/bin"∂∂∂
+export PATH="$PATH:$(go env GOPATH)/bin"
 
 protoc --proto_path='protobuf/' --go_out=plugins=grpc:genproto/go/ protobuf/*
 protoc --proto_path='protobuf/' --csharp_out=genproto/cs/ protobuf/*
