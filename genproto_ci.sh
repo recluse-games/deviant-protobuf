588bf61032b6d05d6b559c564dc47e369ad6856f
cmdallas <self@chrisdallas.tech>
2020-05-02 19:33:27 -0700

    ci(genproto_ci): remove PB_REL

--- genproto_ci.sh
+++ genproto_ci.sh
@@ -4,9 +4,10 @@
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 sudo curl -LO $PB_REL/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
 sudo unzip protoc-3.11.4-linux-x86_64.zip -d $HOME/.local
+rm -f ${PB_REL}
 export PATH="$PATH:$HOME/.local/bin"
 go get github.com/golang/protobuf/protoc-gen-go
-export PATH="$PATH:$(go env GOPATH)/bin"
+export PATH="$PATH:$(go env GOPATH)/bin"∂∂∂
 
 protoc --proto_path='protobuf/' --go_out=plugins=grpc:genproto/go/ protobuf/*
 protoc --proto_path='protobuf/' --csharp_out=genproto/cs/ protobuf/*
